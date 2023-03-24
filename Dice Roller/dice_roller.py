d7997d401a6419308ffb2149d3897c0ca04491a9
Alpha <rohithss2001@gmail.com>
2023-03-24 07:08:01 +0530

    Modified : code

--- Dice Roller/dice_roller.py
+++ Dice Roller/dice_roller.py
@@ -1,14 +1,14 @@
 import random
-print("Dice Rolling\n")
-print("Enter 1 to start and 0 to end the game")
+print("Welcome to the simple dice roller program.\n")
+
 while True:
-    n=int(input("Enter : "))
+    n=int(input("Enter 1 to start rolling the dice or 0 to end: "))
     if(n==1):
         number=random.randint(1,6)
-        print("Die landed is ",number)
+        print("The dice landed on: ",number)
         print("---------------")
     elif(n==0):
-        print("Game ended")
+        print("Thanks for playing!")
         break
     else:
         print("Enter valid number")
